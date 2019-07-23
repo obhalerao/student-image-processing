d2c8278084d75703416aa9f9e9ac869d654b39b8
Omkar Bhalerao <obhal6543@gmail.com>
2019-07-23 15:35:15 -0400

    made more changes to analyze_iamge

--- analyzeimage.py
+++ analyzeimage.py
@@ -3,14 +3,17 @@ import numpy as np
 
 def analyzeimage(old_image):
 
-        green_img = None #do some stuff to the image to get the green
-        nir_img = None #do some stuff to the image to get the nir
+        old_image_arr = np.array(old_image)
+
+        nir_img = np.array[2]
+
+        green_img = np.array[1] - nir_img
 
         built_up_index = (green_img-2*nir_img)/(green_img+2*nir_img)
 
-        built_up_index = cv2.fromarray(built_up_index)
+        built_up_image = cv2.fromarray(built_up_index)
 
-        cv2.imwrite('static/temp.jpg', built_up_index)
+        cv2.imwrite('static/temp.jpg', built_up_image)
         
 	# analyze the image 'old_image' here
 	# save the analyzed image to 'static/temp.jpg'
