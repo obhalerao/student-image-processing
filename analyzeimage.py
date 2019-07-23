b0572368f41759b561ee9a2ab9f9d4d6e0182ef7
Omkar Bhalerao <obhal6543@gmail.com>
2019-07-23 14:08:39 -0400

    Started work on analyzeimage.py

--- analyzeimage.py
+++ analyzeimage.py
@@ -1,6 +1,17 @@
-
+import cv2
+import numpy as np
 
 def analyzeimage(old_image):
+
+        green_img = None #do some stuff to the image to get the green
+        nir_img = None #do some stuff to the image to get the nir
+
+        built_up_index = (green_img-2*nir_img)/(green_img+2*nir_img)
+
+        built_up_index = cv2.fromarray(built_up_index)
+
+        cv2.imwrite('static/temp.jpg', built_up_index)
+        
 	# analyze the image 'old_image' here
 	# save the analyzed image to 'static/temp.jpg'
 	# return the path of the new image file
