bd86f1ed58fdb88790b536830324066707151e56
Omkar Bhalerao <obhal6543@gmail.com>
2019-07-23 09:58:14 -0400

    Used pathlib and added images

--- ImageStitching.py
+++ ImageStitching.py
@@ -1,27 +1,20 @@
-from imutils import paths
+import pathlib as pl
 import numpy as np
 import imutils
 import cv2
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", type=str, required=True,
-	help="path to input directory of images to stitch")
-ap.add_argument("-o", "--output", type=str, required=True,
-	help="path to the output image")
-ap.add_argument("-c", "--crop", type=int, default=0,
-	help="whether to crop out largest rectangular region")
-args = vars(ap.parse_args())
+filepath = 'random-images'
 
 
-print("[INFO] loading images...")
-imagePaths = sorted(list(paths.list_images(args["images"])))
+
+imagePaths = pl.Path(filepath).iterdir()
 images = []
 
 
 for imagePath in imagePaths:
-	image = cv2.imread(imagePath)
-	images.append(image)
+        image = cv2.imread(str(imagePath))
+        images.append(image)
 
 print("[INFO] stitching images...")
 stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
